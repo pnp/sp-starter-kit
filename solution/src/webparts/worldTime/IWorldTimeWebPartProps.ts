bc0fb216c1b846a24798c2f7bfbfe1ade82c6593
paolo@pialorsi.com <paolo@pialorsi.com>
2018-04-04 11:51:26 +0200

    Implemented WorldTime Web Part

---
+++ solution/src/webparts/worldTime/IWorldTimeWebPartProps.ts
@@ -0,0 +1,4 @@
+export interface IWorldTimeWebPartProps {
+    description: string;
+    timeZoneOffset: number;
+}
--- solution/src/webparts/worldTime/loc/mystrings.d.ts
+++ solution/src/webparts/worldTime/loc/mystrings.d.ts
@@ -1,7 +1,19 @@
 declare interface IWorldTimeWebPartStrings {
+
+  // Property Pane labels and strings
   PropertyPaneDescription: string;
   BasicGroupName: string;
   DescriptionFieldLabel: string;
+  TimeZoneOffsetFieldLabel: string;
+
+  // Placeholder labels and strings
+  PlaceholderIconName: string;
+  PlaceholderIconText: string;
+  PlaceholderDescription: string;
+  PlaceholderButtonLabel: string;
+
+  // Default strings for UI
+  LocalTimeDescription: string;
 }
 
 declare module 'WorldTimeWebPartStrings' {
