3ca3a838cd8433f8d03b9eab89133e7645330a4b
Elio Struyf <info@estruyf.be>
2018-05-15 09:52:06 +0200

    Suppression and styling fix

--- solution/gulpfile.js
+++ solution/gulpfile.js
@@ -3,13 +3,14 @@
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+build.addSuppression("Warning - [sass] The local CSS class '-webkit-filter' is not camelCase and will not be type-safe.");
 build.addSuppression(/Admins can make this solution available to all sites in the organization/);
 build.addSuppression(/Warning - Admins can make this solution available to all sites immediately/);
 
 // build.configureWebpack.mergeConfig({
 //     additionalConfiguration: (generatedConfiguration) => {
 //       generatedConfiguration.module.rules.push({
-//         test: /\.less$/,   
+//         test: /\.less$/,
 //         use: [
 //           {
 //             loader: 'css-loader'
@@ -19,7 +20,7 @@ build.addSuppression(/Warning - Admins can make this solution available to all s
 //           },
 //         ],
 //       });
-  
+
 //       return generatedConfiguration;
 //     }
 //   });
