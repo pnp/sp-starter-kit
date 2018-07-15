fdea139d0647229c9aad6dcb53a484e37a48dda2
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:39:53 -0700

    Create es-es.js

---
+++ solution/src/webparts/personalEmail/loc/es-es.js
@@ -0,0 +1,10 @@
+define([], function() {
+  return {
+    "Error": "Un erorr ha ocurrido mientrar obtenemos sus mensajes",
+    "Loading": "Obteniendo sus mensajes",
+    "NoMessages": "No se encontraron mensajes",
+    "NrOfMessagesToShow": "Numero de mensajes a mostrar",
+    "PropertyPaneDescription": "Configuracion del elemento web de Mensajes",
+    "ViewAll": "Ver la lista completa"
+  }
+});
