d7310a842bb32b98a776cc8b33cc95f2bfcbbe19
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:41:25 -0700

    Create es-es.js

---
+++ solution/src/webparts/personalTasks/loc/es-es.js
@@ -0,0 +1,13 @@
+define([], function() {
+  return {
+    "Completed": "Completado",
+    "Error": "Un error ha ocurrido mientras obtenemos sus tareas",
+    "InProgress": "En progreso",
+    "Loading": "Obteniendo sus tareas",
+    "NoTasks": "No se encontraron tareas",
+    "NotStarted": "No iniciada",
+    "ShowCompleted": "Mostrar tareas completadas",
+    "PropertyPaneDescription": "Configuracion de elemento web de Tareas",
+    "ViewAll": "Ver la lista completa"
+  }
+});
