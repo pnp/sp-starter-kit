16ef0fea4b87b78f22aed592792769de6d721243
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:47:11 -0700

    Create es-es.js

---
+++ solution/src/webparts/recentlyVisitedSites/loc/es-es.js
@@ -0,0 +1,7 @@
+define([], function() {
+  return {
+    "NoRecentSitesMsg": "No encontramos sitios utilizados recientemente.",
+    "Loading": "Obteniendo los sitios utilizados recientemente.",
+    "Error": "Lo sentimos, algo fallo cuando obteniamos sus sitios recientemente utilizados."
+  }
+});
