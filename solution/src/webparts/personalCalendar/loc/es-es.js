ebf5c7d7383600f1f5a52165d64a5823e0d674f2
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:37:14 -0700

    Create es-es.js

---
+++ solution/src/webparts/personalCalendar/loc/es-es.js
@@ -0,0 +1,16 @@
+define([], function() {
+  return {
+    "AllDay": "Todo el día",
+    "DaysInAdvance": "Cuantos días por adelantado necesita retornar reuniones? 0 - solo hoy",
+    "Error": "Ha ocurrido un error mientras retornabamos sus proximos eventos",
+    "Hour": "hora",
+    "Hours": "horas",
+    "Loading": "Cargando sus proxima reuniones",
+    "Minutes": "minutos",
+    "NoMessages": "Usted no cuenta con proximas reuniones",
+    "NumMeetings": "CCuantas reuniones mostrar? 0 - mostrar todas las reuniones obtenidas",
+    "RefreshInterval": "Que tan frecuente revisar sus proximas reuniones (en minutos)",
+    "PropertyPaneDescription": "Configuracion del elemento web de reuniones",
+    "ViewAll": "Ver la lista completa"
+  }
+});
