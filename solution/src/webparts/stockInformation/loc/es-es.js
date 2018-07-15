cb226f6050ad694e0f8a523f97f232197095991e
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:52:01 -0700

    Create es-es.js

---
+++ solution/src/webparts/stockInformation/loc/es-es.js
@@ -0,0 +1,17 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Información de la Bolsa",
+    "BasicGroupName": "Configuración General",
+    "StockSymbolFieldLabel": "Código de la bolsa",
+    "AutoRefreshFieldLabel": "Actualización automática",
+    
+    "PlaceholderIconName":"Editar",
+    "PlaceholderIconText":"Configuración de elemento web",
+    "PlaceholderDescription":"Favor de configurar este elemeneto web.",
+    "PlaceholderButtonLabel":"Configure",
+
+    "LoadingDataLabel": "Cargarndo datos ...",
+    "NoDataForStockSymbol":"No se encontro información de la bolsa para: ",
+    "NoAPIKeyInTenantProperties":"No se cuenta con llave de API en las propiedades de la suscripcion. Favor de configurar la solucion apropiadamente."
+  }
+});
