95b88fca691a60d29fddfdbf4223015254030e49
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:00:17 -0700

    Create es-es.js

---
+++ solution/src/webparts/followedSites/loc/es-es.js
@@ -0,0 +1,16 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Configuración del elemento web",
+
+    "NrOfFollowedItemsLabel": "Número de sitios seguidos a retornar",
+    "SortOrderFollowedItemsLabel": "Especifique el método de ordenacion de los sitios retornados",
+    "SortOrderDefaultLabel": "Predeterminado",
+    "SortOrderNameLabel": "Nombre",
+
+    "NoFollowedSitesMsg": "Usted aun no sigue ningun sitio",
+    "SitesFilterLabel": "Filtre sitios por nombre",
+    "NoFollowSitesFoundMsg": "No se encontraron sitios que correspondan",
+    "loading": "Cargando sitios que usted sigue",
+    "error": "Lo sentimos, algo salio mal cuando retornabamos los siios que suted sigue."
+  }
+});
