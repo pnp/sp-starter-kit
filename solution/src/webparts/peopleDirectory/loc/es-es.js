95b405861839c01fd8d3f27f4c7d647f07a39414
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:22:40 -0700

    Create es-es.js

---
+++ solution/src/webparts/peopleDirectory/loc/es-es.js
@@ -0,0 +1,9 @@
+define([], function() {
+  return {
+    "SearchButtonText": "Buscar",
+    "LoadingSpinnerLabel": "Cargando directorio de personas...",
+    "NoPeopleFoundLabel": "No se ha encontrado personas",
+    "SearchBoxPlaceholder": "Buscar",
+    "Error": "Error"
+  }
+});
