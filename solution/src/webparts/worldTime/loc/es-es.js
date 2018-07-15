9a96d6f229253a119c0d397682d4bb479db485d3
HelpDesk SharePoint <support@helpdesk-sharepoint.com>
2018-07-14 20:58:48 -0700

    Create es-es.js

---
+++ solution/src/webparts/worldTime/loc/es-es.js
@@ -0,0 +1,15 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "La hora mundial",
+    "BasicGroupName": "Configuracion general",
+    "DescriptionFieldLabel": "Descrpocion",
+    "TimeZoneOffsetFieldLabel": "Zona de Horario",
+
+    "PlaceholderIconName":"Editar",
+    "PlaceholderIconText":"Configure su elemento web",
+    "PlaceholderDescription":"Favor de configurar el elemento web.",
+    "PlaceholderButtonLabel":"Configurar",
+
+    "LocalTimeDescription": "Tiempo UTC"
+  }
+});
