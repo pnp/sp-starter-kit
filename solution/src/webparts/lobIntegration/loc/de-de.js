71762d40d15668d2e435255b5d4f9226eb3e5e1b
E:\Dev\GitHub\gszdev\sp-starter-kit\solution <stefan.zaeh@gabo.de>
2018-09-26 16:29:05 +0200

    Added localization files for german language (de-de)

---
+++ solution/src/webparts/lobIntegration/loc/de-de.js
@@ -0,0 +1,31 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "LOB Integration",
+    "BasicGroupName": "Allgemeine Einstellungen",
+    "WebApiUriFieldLabel": "Web API URI",
+    "FunctionUriFieldLabel": "Funktion URI",
+    "ServiceTypeFieldLabel": "Diensttyp",
+
+    "PlaceholderIconName":"Ändern",
+    "PlaceholderIconText":"Konfigurieren Sie Ihren Web Part",
+    "PlaceholderDescription":"Bitte konfigurieren Sie den Web Part",
+    "PlaceholderButtonLabel":"Konfigurieren",
+
+    "LoadingDataLabel": "Lade Daten ...",
+    "SearchFor": "Suche nach:",
+    "SearchButtonText": "Suche",
+    "SearchDescription": "Suche nach Kunden im LOB System",
+    "ListButtonText": "Kunden auflisten",
+    "ListDescription": "Listet Kunden aus dem LOb System auf",
+
+    "CustomerIDColumn": "Customer ID",
+    "CustomerIDColumnAriaLabel": "Kunden ID",
+    "CompanyNameColumn": "Company",
+    "CompanyNameColumnAriaLabel": "Firma",
+    "ContactNameColumn": "Contact",
+    "ContactNameColumnAriaLabel": "Kontakt",
+    "CountryColumn": "Country",
+    "CountryColumnAriaLabel": "Land",
+  
+  }
+});
---
+++ solution/src/webparts/worldTime/loc/de-de.js
@@ -0,0 +1,15 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Welt Zeit",
+    "BasicGroupName": "Allgemeine Einstellungen",
+    "DescriptionFieldLabel": "Beschreibung",
+    "TimeZoneOffsetFieldLabel": "Zeitzone",
+
+    "PlaceholderIconName":"Ändern",
+    "PlaceholderIconText":"Konfigurieren Sie Ihren Web Part",
+    "PlaceholderDescription":"Bitte konfigurieren Sie Ihren Web Part.",
+    "PlaceholderButtonLabel":"Konfiguration",
+
+    "LocalTimeDescription": "UTC Zeit"
+  }
+});
