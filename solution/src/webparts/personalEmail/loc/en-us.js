9364a5fc74eb4573ea53d1d9dea8375caf0d8218
Daniel Laskewitz <daniel.laskewitz@sogeti.com>
2018-10-03 20:42:43 +0200

    Added New e-mail and New meeting links at the top of the personal e-mail and personal calendar webparts

--- solution/src/webparts/personalEmail/loc/en-us.js
+++ solution/src/webparts/personalEmail/loc/en-us.js
@@ -2,6 +2,7 @@ define([], function() {
   return {
     "Error": "An error has occurred while retrieving your messages",
     "Loading": "Retrieving your messages",
+    "NewEmail": "New e-mail",
     "NoMessages": "No messages were found",
     "NrOfMessagesToShow": "Number of messages to show",
     "PropertyPaneDescription": "Messages web part configuration",
