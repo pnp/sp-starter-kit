94de3d23ca77799afbc84ffd072df98eaf1a9f18
Daniel Laskewitz <daniel.laskewitz@sogeti.com>
2018-10-09 19:37:28 +0200

    Update nl-nl.js

--- solution/src/webparts/recentlyVisitedSites/loc/nl-nl.js
+++ solution/src/webparts/recentlyVisitedSites/loc/nl-nl.js
@@ -2,7 +2,7 @@ define([], function() {
     return {
       "NoRecentSitesMsg": "We hebben geen recent gebruikte sites gevonden.",
       "Loading": "Recent gebruikte sites aan het ophalen.",
-      "Error": "Sorry, er is iets foutgegaan bij het ophalen van je recent gebruikte sites."
+      "Error": "Sorry, er is iets fout gegaan bij het ophalen van je recent gebruikte sites."
     }
   });
   
