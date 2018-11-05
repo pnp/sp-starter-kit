94b350753b362ede9fad5164981fda5bfb942b5a
Daniel Laskewitz <daniel.laskewitz@sogeti.com>
2018-11-05 21:22:29 +0100

    Added dutch language for webparts and extensions

---
+++ solution/src/webparts/links/loc/nl-nl.js
@@ -0,0 +1,26 @@
+define([], function() {
+    return {
+      "groupDataLabel": "Groepnamen voor de links",
+      "groupPanelHeader": "Configureer je groepnamen",
+      "manageGroupBtn": "Configureer groepen",
+      "iconInformation": "De icoonnamen kan je vinden via de volgende URL:",
+  
+      "linkDataLabel": "Link data",
+      "linkPanelHeader": "Configureer je links",
+      "manageLinksBtn": "Configureer links",
+  
+      "titleField": "Titel",
+      "urlField": "URL",
+      "iconField": "UI Fabric icoonnaam",
+      "groupField": "Groupnaam",
+      "targetField": "Doel",
+  
+      "targetCurrent": "Huidig venster",
+      "targetNew": "Nieuw tabblad",
+  
+      "noLinksIconText": "Configureer je links",
+      "noLinksConfigured": "Configureer het webpart om links te tonen",
+      "noLinksBtn": "Configureer",
+    }
+  });
+  
---
+++ solution/src/webparts/siteInformation/loc/nl-nl.js
@@ -0,0 +1,19 @@
+define([], function() {
+    return {
+      "PropertyPaneDescription": "Omschrijving",
+      "BasicGroupName": "Groepsnaam",
+      "SiteTitleFieldLabel": "Site Titel",
+      "SiteContactFieldLabel": "Site Contactpersoon",
+      "SiteOrganizationFieldLabel": "Site Organisatie",
+      "SiteOrganizationPanelTitle": "Site Organisatie",
+  
+      "PlaceholderIconName":"Bewerken",
+      "PlaceholderIconText":"Configureer je webpart",
+      "PlaceholderDescription":"Configureer het webpart.",
+      "PlaceholderButtonLabel":"Configureer",
+  
+      "SiteTitleCaption": "Site Titel:",
+      "SiteContactCaption": "Site Contactpersoon:",
+      "SiteOrganizationCaption": "Site Organizatie:"
+    }
+  });
