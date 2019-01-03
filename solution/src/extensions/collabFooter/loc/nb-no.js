b2179cb4b6aa516a36a8e71afe491529fd9f1253
Mikael Svenson <miksvenson@gmail.com>
2019-01-03 09:11:07 +0100

    Update nb-no.js
    Endre til lowercase i ord #2 i tittel

--- solution/src/extensions/collabFooter/loc/nb-no.js
+++ solution/src/extensions/collabFooter/loc/nb-no.js
@@ -1,7 +1,7 @@
 define([], function() {
   return {
     "Title": "CollabFooterApplicationCustomizer",
-    "MyLinks": "Mine Lenker",
+    "MyLinks": "Mine lenker",
     "EditMyLinks": "Rediger mine lenker",
     "MyLinksSaveSuccess": "Mine lenker har blitt lagret!",
     "MyLinksSaveFailed": "Lagring av mine lenker feilet!"
