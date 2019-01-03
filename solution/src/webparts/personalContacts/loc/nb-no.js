f19b6bbbd7f199ec8782cc7cf32b9d20d411e2a4
Mikael Svenson <miksvenson@gmail.com>
2019-01-03 09:20:44 +0100

    Update nb-no.js
    Language updates

--- solution/src/webparts/personalContacts/loc/nb-no.js
+++ solution/src/webparts/personalContacts/loc/nb-no.js
@@ -3,7 +3,7 @@ define([], function() {
     "Error": "En feil oppstod ved henting av dine kontakter",
     "Loading": "Henter dine kontakter",
     "NoContacts": "Ingen kontakter funnet",
-    "NrOfContactsToShow": "Antal kontakter som skal vises",
+    "NrOfContactsToShow": "Antall kontakter som skal vises",
     "PropertyPaneDescription": "Konfigurer kontakter webdelen",
     "ViewAll": "Vis hele listen"
   }
