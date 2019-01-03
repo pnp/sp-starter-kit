91c94c434d8b32e64525ddc25ec1a7babff4a811
Mikael Svenson <miksvenson@gmail.com>
2019-01-03 09:16:56 +0100

    Update nb-no.js
    Language updates

--- solution/src/webparts/followedSites/loc/nb-no.js
+++ solution/src/webparts/followedSites/loc/nb-no.js
@@ -2,8 +2,8 @@ define([], function() {
   return {
     "PropertyPaneDescription": "Webdel konfigurasjon",
 
-    "NrOfFollowedItemsLabel": "Antal fulgte områder å hente",
-    "SortOrderFollowedItemsLabel": "Spesifiser sorteringen for områdene",
+    "NrOfFollowedItemsLabel": "Antall fulgte områder som hentes",
+    "SortOrderFollowedItemsLabel": "Spesifiser sortering for områdene",
     "SortOrderDefaultLabel": "Standard",
     "SortOrderNameLabel": "Navn",
 
@@ -11,6 +11,6 @@ define([], function() {
     "SitesFilterLabel": "Filtrer områder etter navn",
     "NoFollowSitesFoundMsg": "Ingen områder funnet",
     "loading": "Laster inn fulgte områder",
-    "error": "Dessverre, noe gikk galt med lastingen av dine fulgte områder."
+    "error": "Dessverre gikk noe galt med innlastingen av dine fulgte områder."
   }
 });
