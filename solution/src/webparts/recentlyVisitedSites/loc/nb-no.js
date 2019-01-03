37b502727e2c2f1255ec92c3ac3a9e813a0e222f
Mikael Svenson <miksvenson@gmail.com>
2019-01-03 09:24:50 +0100

    Update nb-no.js
    Language update

--- solution/src/webparts/recentlyVisitedSites/loc/nb-no.js
+++ solution/src/webparts/recentlyVisitedSites/loc/nb-no.js
@@ -2,6 +2,6 @@ define([], function() {
   return {
     "NoRecentSitesMsg": "Vi fant ingen sist besøkte områder.",
     "Loading": "Henter dine sist besøkte områder.",
-    "Error": "Dessverre, noe gikk feil ved henting av dine sist besøkte områder."
+    "Error": "Noe gikk galt ved henting av dine sist besøkte områder."
   }
 });
