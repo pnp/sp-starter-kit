6ad935dd3d6e99baf7a2c5a4178e3edd5eb85005
Mikael Svenson <miksvenson@gmail.com>
2019-01-03 09:14:16 +0100

    Update nb-no.js
    Language updates

--- solution/src/extensions/portalFooter/loc/nb-no.js
+++ solution/src/extensions/portalFooter/loc/nb-no.js
@@ -1,12 +1,12 @@
 define([], function () {
   return {
     "Title": "PortalFooterApplicationCustomizer",
-    "MyLinks": "Mina Lenker",
+    "MyLinks": "Mine lenker",
     "ToggleButtonOpen": "Åpne",
     "ToggleButtonClose": "Lukke",
     "Edit": "Rediger",
     "EditTitle": "Velg lenker",
-    "MyLinksSaveSuccess": "Mina lenker har blitt lagret!",
+    "MyLinksSaveSuccess": "Mine lenker har blitt lagret!",
     "MyLinksSaveFailed": "Klarte ikke å lagre mine lenker!"
   }
 });
