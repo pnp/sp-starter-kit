2e3580a33a701b4250e2464edb5dbd9f40e1ccb4
Beau Cameron <decathlon.beau@gmail.com>
2019-11-26 10:11:29 -0700

    Splitting out web parts for V2

---
+++ source/react-personal-calendar/src/webparts/personalCalendar/loc/tr-tr.js
@@ -0,0 +1,17 @@
+define([], function() {
+  return {
+    "AllDay": "Tüm gün",
+    "DaysInAdvance": "Kaç gün önceden öne almak istiyorsunuz? 0 - sadece bugün",
+    "Error": "Yaklaşan toplantılarınız alınırken bir hata oluştu",
+    "Hour": "saat",
+    "Hours": "saat",
+    "Loading": "Yaklaşan toplantınız alınıyor",
+    "Minutes": "dakika",
+    "NewMeeting": "Yeni toplantı",
+    "NoMessages": "Yaklaşan toplantınız yok",
+    "NumMeetings": "Gösterilecek kaç toplantı var? 0 - tüm alınan toplantıları göster",
+    "RefreshInterval": "Yaklaşan yeni toplantıların ne sıklıkta kontrol edileceği (dakika olarak)",
+    "PropertyPaneDescription": "Toplantılar web bölümü yapılandırması",
+    "ViewAll": "Tam listeyi görüntüle"
+  }
+});
