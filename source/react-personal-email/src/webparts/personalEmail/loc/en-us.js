072e685674e57ce55a6e5b58ac29f8a8c4b44413
Beau Cameron <decathlon.beau@gmail.com>
2019-11-29 16:05:20 -0700

    Moved V1 to V2

--- source/react-personal-email/src/webparts/personalEmail/loc/en-us.js
+++ source/react-personal-email/src/webparts/personalEmail/loc/en-us.js
@@ -1,7 +1,13 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field"
+    "Error": "An error has occurred while retrieving your messages",
+    "Loading": "Retrieving your messages",
+    "NewEmail": "New e-mail",
+    "NoMessages": "No messages were found",
+    "NrOfMessagesToShow": "Number of messages to show",
+    "PropertyPaneDescription": "Messages web part configuration",
+    "ShowInboxOnly":"Show e-mail from Inbox only",
+    "ShowInboxOnlyCallout": "When selected, only e-mails from Inbox will be shown.",
+    "ViewAll": "View the full list"
   }
 });
---
+++ source/react-personal-email/src/webparts/personalEmail/loc/tr-tr.js
@@ -0,0 +1,13 @@
+define([], function() {
+  return {
+    "Error": "Mesajlarınız alınırken bir hata oluştu.",
+    "Loading": "Mesajlarınızı alınıyor",
+    "NewEmail": "Yeni e-posta",
+    "NoMessages": "Hiçbir mesaj bulunamadı",
+    "NrOfMessagesToShow": "Gösterilecek mesaj sayısı",
+    "PropertyPaneDescription": "Mesajlar web parçası yapılandırması",
+    "ShowInboxOnly":"Sadece gelen kutusundaki e-postayı göster",
+    "ShowInboxOnlyCallout": "Seçildiğinde, yalnızca Gelen Kutusu'ndan gelen e-postalar gösterilecektir.",
+    "ViewAll": "Listenin tamamını görüntüle"
+  }
+});
