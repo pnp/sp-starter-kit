72a760a2740f70b3190982bb6c1cbf2d90a2f764
Paul Schaeflein <paul@schaeflein.net>
2019-12-02 23:42:59 -0600

    Initial commit of v2 LOB solution

---
+++ sample-lob-service/SharePointPnP.LobScenario/Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.LobScenario.Models
+{
+	public class Customer
+	{
+		public string CustomerID { get; set; }
+		public string CompanyName { get; set; }
+		public string ContactName { get; set; }
+		public string Country { get; set; }
+	}
+}
---
+++ sample-lob-service/SharePointPnP.LobScenario/Models/LobResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharePointPnP.LobScenario.Models
+{
+	public class LobResponse
+	{
+		public String Username { get; set; }
+
+		public DateTime RequestDateTime { get; set; }
+
+		public IEnumerable<Customer> Customers { get; set; }
+	}
+}
