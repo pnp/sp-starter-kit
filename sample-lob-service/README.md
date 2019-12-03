f381f024c3f3a7fdfd4ccb02a2013584b2f14913
Paul Schaeflein <paul@schaeflein.net>
2019-12-02 23:42:59 -0600

    Initial commit of v2 LOB solution

--- sample-lob-service/README.md
+++ sample-lob-service/README.md
@@ -1,4 +1,4 @@
 
 # PnP SharePoint Starter Kit - LOB Service
 
-This folder contains an ASP.NET MVC Web API application that emulates a LOB system, which you can host on Microsoft Azure (or wherever you like) and consume through the LobIntegration sample client-side Web Part defined in the PnP Starter Kit project. Further instruction will come about how to setup this LOB service and about how to configure the LobIntegration Web Part.
+This folder contains an ASP.NET Core WebAPI application that emulates a LOB system, which you can consume through the LobIntegration sample client-side Web Part defined in the PnP Starter Kit project. Refer to the [react-lob-integration web part](../source/react-lob-integration/README.md) for setup instructions.
---
+++ sample-lob-service/SharePointPnP.LobScenario/Controllers/CustomersController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SharePointPnP.LobScenario.Models;
+
+namespace SharePointPnP.LobScenario.Controllers
+{
+	[Authorize]
+	[ApiController]
+	[Route("[controller]")]
+	public class CustomersController : ControllerBase
+	{
+		private readonly ILogger<CustomersController> _logger;
+
+		public CustomersController(ILogger<CustomersController> logger)
+		{
+			_logger = logger;
+		}
+
+		// GET /customers
+		[HttpGet]
+		public LobResponse Get()
+		{
+			return (new LobResponse
+			{
+				Username = HttpContext.User.Identities.FirstOrDefault()?.Name,
+				RequestDateTime = DateTime.Now,
+				Customers = Customers,
+			});
+		}
+
+		// GET /customers/{customerId}
+		[HttpGet]
+		[Route("{customerId}")]
+		public LobResponse Get(string customerId)
+		{
+			return (new LobResponse
+			{
+				Username = HttpContext.User.Identities.FirstOrDefault()?.Name,
+				RequestDateTime = DateTime.Now,
+				Customers = Customers.Where(c => c.CustomerID.ToLowerInvariant() == customerId.ToLowerInvariant())
+			});
+		}
+
+		// GET /customers/search/Morgenstern Gesundkost
+		[HttpGet]
+		[Route("search/{searchValue}")]
+		public Models.LobResponse Search(string searchValue)
+		{
+			return (new LobResponse
+			{
+				Username = HttpContext.User.Identities.FirstOrDefault()?.Name,
+				RequestDateTime = DateTime.Now,
+				Customers = Customers.Where(c => c.CustomerID.ToLowerInvariant() == searchValue.ToLowerInvariant() ||
+																				 c.CompanyName.Contains(searchValue, StringComparison.InvariantCultureIgnoreCase))
+														 .ToList()
+			});
+		}
+
+		private readonly List<Customer> Customers = new List<Customer> {
+			new Customer{ CustomerID = "ALFKI", CompanyName = "Alfreds Futterkiste",ContactName = "Maria Anders",Country = "Germany"            },
+			new Customer{ CustomerID = "ANATR", CompanyName = "Ana Trujillo Emparedados y helados", ContactName = "Ana Trujillo", Country = "Mexico" },
+			new Customer{ CustomerID = "ANTON", CompanyName = "Antonio Moreno Taquería", ContactName = "Antonio Moreno", Country = "Mexico" },
+			new Customer{ CustomerID = "AROUT", CompanyName = "Around the Horn", ContactName = "Thomas Hardy", Country = "UK" },
+			new Customer{ CustomerID = "BERGS", CompanyName = "Berglunds snabbköp", ContactName = "Christina Berglund", Country = "Sweden" },
+		};
+	}
+}
