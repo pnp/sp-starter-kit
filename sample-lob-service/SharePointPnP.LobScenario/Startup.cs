1bda4195aed9a9aea84ccd5bec081f0c3f74ec4b
Paul Schaeflein <paul@schaeflein.net>
2019-12-04 11:04:44 -0600

    WebApi updates for CORS and settings

--- sample-lob-service/SharePointPnP.LobScenario/Startup.cs
+++ sample-lob-service/SharePointPnP.LobScenario/Startup.cs
@@ -73,7 +73,7 @@ namespace SharePointPnP.LobScenario
 			app
 				.UseHttpsRedirection()
 				.UseRouting()
-				.UseCors()
+				.UseCors("AllowSubdomain")
 				.UseAuthentication()
 				.UseAuthorization();
 
