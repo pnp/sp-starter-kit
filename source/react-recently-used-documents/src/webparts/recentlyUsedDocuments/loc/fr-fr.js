d2d0270c1b8b8a580addfd7d765df50c43423740
Sébastien Levert <slevert@outlook.com>
2019-12-29 18:39:05 -0500

    Added localizations

---
+++ source/react-recently-used-documents/src/webparts/recentlyUsedDocuments/loc/fr-fr.js
@@ -0,0 +1,20 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Configuration de la partie Web",
+    "NrOfDocumentsToShow": "Spécifiez le nombre de documents à afficher",
+
+    "NoRecentDocuments": "Nous n'avons trouvé aucun document.",
+    "LastUsedMsg": "Dernier accès:",
+    "Loading": "Récupération de vos documents récemment utilisés",
+    "Error": "Désolé, une erreur s'est produite lors de la récupération des documents récemment utilisés.",
+
+    "DateJustNow": "juste maintenant",
+    "DateMinute": "il y a 1 minutes",
+    "DateMinutesAgo": "il y a quelques minutes",
+    "DateHour": "il y a une heure",
+    "DateHoursAgo": "il y a quelques heures",
+    "DateDay": "hier",
+    "DateDaysAgo": "il y a quelques jours",
+    "DateWeeksAgo": "il y a quelques semaines"
+  }
+});
