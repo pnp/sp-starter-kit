d00974c191df233ddfabde6d55e58b2ccb6ee1c1
Sébastien Levert <slevert@outlook.com>
2019-12-29 18:22:16 -0500

    Initial commit of Recently Used Documents

--- source/react-recently-used-documents/src/webparts/recentlyUsedDocuments/components/IRecentlyUsedDocumentsProps.ts
+++ source/react-recently-used-documents/src/webparts/recentlyUsedDocuments/components/IRecentlyUsedDocumentsProps.ts
@@ -1,3 +1,11 @@
-export interface IRecentlyUsedDocumentsProps {
-  description: string;
+import { IRecentlyUsedDocumentsWebPartProps } from '../RecentlyUsedDocumentsWebPart';
+import { DisplayMode } from '@microsoft/sp-core-library';
+import { MSGraphClient } from '@microsoft/sp-http';
+import { WebPartContext } from '@microsoft/sp-webpart-base';
+
+export interface IRecentlyUsedDocumentsProps extends IRecentlyUsedDocumentsWebPartProps {
+  context: WebPartContext;
+  displayMode: DisplayMode;
+  graphClient: MSGraphClient;
+  updateProperty: (value: string) => void;
 }
