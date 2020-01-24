6ff4e9f68be12cb946787d040e4698da61e1e90f
Fabio Franzini <fabio@fabiofranzini.com>
2020-01-24 12:04:31 +0100

    [v2] React Links

---
+++ source/react-links/src/webparts/links/loc/es-es.js
@@ -0,0 +1,25 @@
+define([], function() {
+  return {
+    "groupDataLabel": "Nombres de grupos para los enlaces",
+    "groupPanelHeader": "Configure los nombres de los grupos",
+    "manageGroupBtn": "Configure grupos",
+    "iconInformation": "Los nombres a especificar para el campo Icono puede encontrarse en la siguiente dirección URL:",
+
+    "linkDataLabel": "Enlace datos",
+    "linkPanelHeader": "Configure sus enlaces",
+    "manageLinksBtn": "Configure enlaces",
+
+    "titleField": "Título",
+    "urlField": "URL",
+    "iconField": "Nombre de Icono de UI Fabric",
+    "groupField": "Nombre del grupo",
+    "targetField": "Destino",
+
+    "targetCurrent": "Ventana actual",
+    "targetNew": "Nueva ventana",
+
+    "noLinksIconText": "Configure sus enlaces",
+    "noLinksConfigured": "Favor de configurar el elemento web a fin de mostrar enlaces",
+    "noLinksBtn": "Configuración",
+  }
+});
--- source/react-links/src/webparts/links/loc/mystrings.d.ts
+++ source/react-links/src/webparts/links/loc/mystrings.d.ts
@@ -1,7 +1,25 @@
 declare interface ILinksWebPartStrings {
-  PropertyPaneDescription: string;
-  BasicGroupName: string;
-  DescriptionFieldLabel: string;
+  groupDataLabel: string;
+  groupPanelHeader: string;
+  manageGroupBtn: string;
+  iconInformation: string;
+
+  linkDataLabel: string;
+  linkPanelHeader: string;
+  manageLinksBtn: string;
+
+  titleField: string;
+  urlField: string;
+  iconField: string;
+  groupField: string;
+  targetField: string;
+
+  targetCurrent: string;
+  targetNew: string;
+
+  noLinksIconText: string;
+  noLinksConfigured: string;
+  noLinksBtn: string;
 }
 
 declare module 'LinksWebPartStrings' {
