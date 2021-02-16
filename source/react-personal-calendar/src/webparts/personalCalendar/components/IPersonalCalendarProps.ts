0843115fd189aaf01d069c4513315deac7c3108e
Will Holland <wholland@threewill.com>
2021-02-16 14:45:56 -0500

    Dropped custom code in favor of MGT templates

--- source/react-personal-calendar/src/webparts/personalCalendar/components/IPersonalCalendarProps.ts
+++ source/react-personal-calendar/src/webparts/personalCalendar/components/IPersonalCalendarProps.ts
@@ -1,6 +1,5 @@
 import { IPersonalCalendarWebPartProps } from "../PersonalCalendarWebPart";
 import { DisplayMode } from "@microsoft/sp-core-library";
-import { MSGraphClient } from "@microsoft/sp-http";
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
 
 export interface IPersonalCalendarProps extends IPersonalCalendarWebPartProps {
