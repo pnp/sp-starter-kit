08810d1f37550b9dbe166da0c3226da0a03e05b2
Don Kirkham <donkirkham@live.com>
2022-02-14 18:56:13 +0000

    clean up last PR for react-personal-contacts

--- source/react-personal-contacts/src/webparts/personalContacts/PersonalContactsWebPart.ts
+++ source/react-personal-contacts/src/webparts/personalContacts/PersonalContactsWebPart.ts
@@ -8,85 +8,85 @@ import { PersonalContacts, IPersonalContactsProps } from './components/PersonalC
 import { MSGraphClient } from '@microsoft/sp-http';
 
 export interface IPersonalContactsWebPartProps {
-	title: string;
-	nrOfContacts: number;
+  title: string;
+  nrOfContacts: number;
 }
 
 export default class PersonalContactsWebPart extends BaseClientSideWebPart<IPersonalContactsWebPartProps> {
-	private graphClient: MSGraphClient;
-	private propertyFieldNumber;
+  private graphClient: MSGraphClient;
+  private propertyFieldNumber;
 
-	public onInit(): Promise<void> {
-		return new Promise<void>((resolve: () => void, reject: (error: any) => void): void => {
-			this.context.msGraphClientFactory
-				.getClient()
-				.then((client: MSGraphClient): void => {
-					this.graphClient = client;
-					resolve();
-				}, err => reject(err));
-		});
-	}
+  public onInit(): Promise<void> {
+    return new Promise<void>((resolve: () => void, reject: (error: any) => void): void => {
+      this.context.msGraphClientFactory
+        .getClient()
+        .then((client: MSGraphClient): void => {
+          this.graphClient = client;
+          resolve();
+        }, err => reject(err));
+    });
+  }
 
-	public render(): void {
-		const element: React.ReactElement<IPersonalContactsProps> = React.createElement(
-			PersonalContacts,
-			{
-				title: this.properties.title,
-				nrOfContacts: this.properties.nrOfContacts,
-				// pass the reference to the MSGraphClient
-				graphClient: this.graphClient,
-				// pass the current display mode to determine if the title should be
-				// editable or not
-				displayMode: this.displayMode,
-				// handle updated web part title
-				updateProperty: (value: string): void => {
-					// store the new title in the title web part property
-					this.properties.title = value;
-				}
-			}
-		);
+  public render(): void {
+    const element: React.ReactElement<IPersonalContactsProps> = React.createElement(
+      PersonalContacts,
+      {
+        title: this.properties.title,
+        nrOfContacts: this.properties.nrOfContacts,
+        // pass the reference to the MSGraphClient
+        graphClient: this.graphClient,
+        // pass the current display mode to determine if the title should be
+        // editable or not
+        displayMode: this.displayMode,
+        // handle updated web part title
+        updateProperty: (value: string): void => {
+          // store the new title in the title web part property
+          this.properties.title = value;
+        }
+      }
+    );
 
-		ReactDom.render(element, this.domElement);
-	}
+    ReactDom.render(element, this.domElement);
+  }
 
-	protected get dataVersion(): Version {
-		return Version.parse('1.0');
-	}
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
 
-	//executes only before property pane is loaded.
-	protected async loadPropertyPaneResources(): Promise<void> {
-		// import additional controls/components
+  //executes only before property pane is loaded.
+  protected async loadPropertyPaneResources(): Promise<void> {
+    // import additional controls/components
 
-		const { PropertyFieldNumber } = await import(
-			/* webpackChunkName: 'pnp-propcontrols-number' */
-			'@pnp/spfx-property-controls/lib/propertyFields/number'
-		);
+    const { PropertyFieldNumber } = await import(
+      /* webpackChunkName: 'pnp-propcontrols-number' */
+      '@pnp/spfx-property-controls/lib/propertyFields/number'
+    );
 
-		this.propertyFieldNumber = PropertyFieldNumber;
-	}
+    this.propertyFieldNumber = PropertyFieldNumber;
+  }
 
-	protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-		return {
-			pages: [
-				{
-					header: {
-						description: strings.PropertyPaneDescription
-					},
-					groups: [
-						{
-							groupFields: [
-								this.propertyFieldNumber("nrOfContacts", {
-									key: "nrOfContacts",
-									label: strings.NrOfContactsToShow,
-									value: this.properties.nrOfContacts,
-									minValue: 1,
-									maxValue: 10
-								})
-							]
-						}
-					]
-				}
-			]
-		};
-	}
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupFields: [
+                this.propertyFieldNumber("nrOfContacts", {
+                  key: "nrOfContacts",
+                  label: strings.NrOfContactsToShow,
+                  value: this.properties.nrOfContacts,
+                  minValue: 1,
+                  maxValue: 10
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
 }
