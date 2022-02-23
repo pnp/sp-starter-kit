241b54d9c9ed9e23cef430dbbef639ee8abce62d
Don <donkirkham@live.com>
2022-02-22 23:26:20 -0600

    update react-people-directory

--- source/react-people-directory/src/webparts/peopleDirectory/loc/en-us.js
+++ source/react-people-directory/src/webparts/peopleDirectory/loc/en-us.js
@@ -1,14 +1,15 @@
 define([], function() {
   return {
-    "SearchButtonText": "Search",
-    "LoadingSpinnerLabel": "Loading people directory...",
-    "NoPeopleFoundLabel": "No people found",
-    "SearchBoxPlaceholder": "Search",
-    "Error": "Error",
-    "SkillsLabel": "Skills",
-    "ProjectsLabel": "Projects",
-    "CopyEmailLabel": "Copy email to clipboard",
-    "CopyPhoneLabel": "Copy phone to clipboard",
-    "CopyMobileLabel": "Copy mobile to clipboard"
-  }
+    SearchButtonText: "Search",
+    LoadingSpinnerLabel: "Loading people directory...",
+    NoPeopleFoundLabel: "No people found",
+    SearchBoxPlaceholder: "Search",
+    Error: "Error",
+    SkillsLabel: "Skills",
+    ProjectsLabel: "Projects",
+    CopyEmailLabel: "Copy email to clipboard",
+    CopyPhoneLabel: "Copy phone to clipboard",
+    CopyMobileLabel: "Copy mobile to clipboard",
+    SearchOnlyLabel: "Hide alphabet (Search only)",
+  };
 });
