adfff85e3e897457acafebe5db9613e131067f25
Chandani Prajapati <chandaniprajapati96@gmail.com>
2023-01-12 10:26:42 +0530

    React-world-clock: Updated gitignore, package, package-solution, manifest, import statement, and teams files.

--- source/react-world-clock/src/webparts/worldClock/WorldClockWebPart.ts
+++ source/react-world-clock/src/webparts/worldClock/WorldClockWebPart.ts
@@ -1,13 +1,8 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
-import {
-  BaseClientSideWebPart,
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField,
-  PropertyPaneDropdown,
-  IPropertyPaneDropdownOption
-} from '@microsoft/sp-webpart-base';
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { IPropertyPaneConfiguration, PropertyPaneTextField, PropertyPaneDropdown, IPropertyPaneDropdownOption } from "@microsoft/sp-property-pane";
 
 import * as strings from 'WorldClockWebPartStrings';
 import WorldClock from './components/WorldClock';
